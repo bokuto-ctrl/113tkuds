5123bb625cb26c382afa6ae3831919eec134450e
bokuto-ctrl <jessica603310@gmail.com>
2025-06-28 16:14:45 +0800

    Create ds_02.java

---
+++ 0626/ds_02.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class ds_02 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("請輸入圓的半徑");
+        double r=sc.nextDouble();
+        double area=Math.PI*r*r;
+        System.out.println("圓的面積:"+area);
+    }
+}
