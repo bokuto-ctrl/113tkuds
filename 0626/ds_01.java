bf2f0bdeb4794eb31b71189f6ad462d2d829a597
bokuto-ctrl <jessica603310@gmail.com>
2025-06-28 15:30:40 +0800

    Update ds_01.java

---
+++ 0626/ds_01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ds_01{
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("請輸入第一個整數");
+        int num1=sc.nextInt();
+        System.out.println("請輸入第二個整數");
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        System.out.println("兩數之和:"+sum);
+    }
+}
