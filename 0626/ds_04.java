2bd53cc6e0ddf56af890ab23d716143acb26fc15
bokuto-ctrl <jessica603310@gmail.com>
2025-06-28 17:03:24 +0800

    Create ds_04.java

---
+++ 0626/ds_04.java
@@ -0,0 +1,10 @@
+public class ds_04 {
+    public static void main(String[] args) {
+        int[]n={10,20,30,40,50};
+        System.out.println("陣列元素為:");
+        for (int i = 0; i <n.length ; i++) {
+            System.out.println("第"+(i+1)+"個元素:"+n[i]);
+            
+        }
+    }
+}
