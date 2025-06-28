55e559d90527faea698118632d959de2d28dce02
bokuto-ctrl <jessica603310@gmail.com>
2025-06-28 17:03:13 +0800

    Update ds_01.java

--- 0626/ds_01.java
+++ 0626/ds_01.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("請輸入第一個整數");
         int num1=sc.nextInt();
-        System.out.println("請輸入第二個整數");
+        System.out.print("請輸入第二個整數");
         int num2=sc.nextInt();
         int sum=num1+num2;
         System.out.println("兩數之和:"+sum);
