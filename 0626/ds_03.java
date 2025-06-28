14db777ca2082fce8a6b544e9db0fb24949082ec
bokuto-ctrl <jessica603310@gmail.com>
2025-06-28 17:03:21 +0800

    Update ds_03.java

--- 0626/ds_03.java
+++ 0626/ds_03.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class ds_03 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("請輸入名字");
+        System.out.print("請輸入名字");
         String name=sc.nextLine();
-        System.out.println("請輸入年齡");
+        System.out.print("請輸入年齡");
         int age=sc.nextInt();
         sc.nextLine();
-        System.out.println("請輸入城市");
+        System.out.print("請輸入城市");
         String city=sc.nextLine();
-        System.out.print("姓名:"+name);
-        System.out.print("年齡:"+age);
-        System.out.print("城市:"+city);
+        System.out.println("姓名:"+name);
+        System.out.println("年齡:"+age);
+        System.out.println("城市:"+city);
     }
 }
