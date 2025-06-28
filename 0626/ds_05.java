9d53a93cb2c4a94235c90cd082fddbeadc2c0224
bokuto-ctrl <jessica603310@gmail.com>
2025-06-28 17:03:26 +0800

    Create ds_05.java

---
+++ 0626/ds_05.java
@@ -0,0 +1,10 @@
+public class ds_05 {
+    public static void main(String[] args) {
+        int[]n={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for(int num:n){
+            sum+=num;
+        }
+        System.out.println("陣列總和為:"+sum);
+    }
+}
