a7e00e1228fee4d652ba98abfcda23405cbcd527
bokuto-ctrl <jessica603310@gmail.com>
2025-07-06 22:51:45 +0800

    Update fibonacciSimple.java

--- 0626/0702/fibonacciSimple.java
+++ 0626/0702/fibonacciSimple.java
@@ -1,12 +1,12 @@
 
-public class fibonacciSimple {
+public class FibonacciSimple {
     public static void main(String[] args) {
-        int n=0;//要印出前幾項
-        int a=0,b=0;
+        int n=10;//要印出前幾項
+        int a=0,b=1;
 
-        System.out.print("費氏數列物"+n+"項");
+        System.out.print("費氏數列前"+n+"項");
         for(int i=0;i<n;i++){
-            System.out.println(a+"");
+            System.out.println(a+" ");
             int temp=a+b;
             a=b;
             b=temp;
