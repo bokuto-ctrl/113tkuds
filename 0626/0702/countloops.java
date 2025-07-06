a5476a15c255cd99e9426f427121e66a08883735
bokuto-ctrl <jessica603310@gmail.com>
2025-07-06 22:51:35 +0800

    Update countloops.java

--- 0626/0702/countloops.java
+++ 0626/0702/countloops.java
@@ -2,6 +2,16 @@
 
 public class countloops {
     public static void main(String[] args) {
-        
+        int n=5;
+        int sum=0;
+        int count=0;//紀錄加法執行次數
+
+        for(int i=1;i<=n;i++){
+            System.out.println("第"+i+"次迴圈,sum="+sum+"+"+i);
+            sum+=i;
+            count++;//每次加法都記錄一次
+        }
+        System.out.println("總合為:"+sum);
+        System.out.println("加法執行了:"+count+"次");
  }
 }
