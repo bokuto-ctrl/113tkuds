949fb1333f7fc9c0e35c367eb8dc6bca6905508a
bokuto-ctrl <jessica603310@gmail.com>
2025-07-15 12:41:40 +0800

    Create Q1_THSRStopCounter.java

---
+++ 0626/期終考試/Q1_THSRStopCounter.java
@@ -0,0 +1,35 @@
+   import java.util.Scanner;
+
+public class Q1_THSRStopCounter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        String[]stops=new String[n];
+
+        for(int i=0;i<n;i++){
+            stops[i]=sc.next();
+        }
+        String start=sc.next();
+        String end=sc.next();
+
+        int startIdx=-1;
+        int endIdx=-1;
+
+        for(int i=0;i<n;i++){
+            if(stops[i].equals(start)){
+                startIdx=i;
+            }
+            if(stops[i].equals(end)){
+                endIdx=i;
+        }
+    }
+    if(startIdx==-1||endIdx==-1){
+        System.out.println("Invalid");
+    }else{
+        int cound=Math.abs(startIdx-endIdx)+1;
+        System.out.println(cound);
+    }
+   
+    }
+}
