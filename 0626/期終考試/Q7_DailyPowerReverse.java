259c26c93a55b36c6fcba89ceb32be6d11887f19
bokuto-ctrl <jessica603310@gmail.com>
2025-07-15 12:41:57 +0800

    Create Q7_DailyPowerReverse.java

---
+++ 0626/期終考試/Q7_DailyPowerReverse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q7_DailyPowerReverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[]power=new int [7];
+
+        for(int i=0;i<7;i++){
+            power[i]=sc.nextInt();
+        }
+        int left=0;
+        int right=6;
+        while (left<right) {
+            int temp=power[left];
+            power[left]=power[right];
+            power[right]=temp;
+            left++;
+            right--;
+        }
+        for(int i=0;i<7;i++){
+            System.out.println(power[i]);
+            if(i<6)System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
